1d1484a77b6009425cfada89f616de62bc1369bc
sagar-modasiya <86204497+sagar-modasiya@users.noreply.github.com>
2025-02-05 08:28:05 +0000

    test1

--- main.c
+++ main.c
@@ -3,11 +3,6 @@
 #include <stdlib.h>
 #include <string.h>
 /**
- * @file main.c
- * @brief This file contains the main function and a helper function to print messages.
- */
-
-/**
  * @brief Prints a message to the standard output.
  *
  * This function takes a string message as input and prints it to the standard output
